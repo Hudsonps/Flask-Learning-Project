b3cb44c0fd55ab65d7f8a91b51aa50bba6ca6e0b
Hudson <hudpsa@gmail.com>
2018-08-20 16:46:12 -0400

    Implemented logout method and interface

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)  #object that represents the database
 migrate = Migrate(app, db) #object that represents the migration engine
+login = LoginManager(app)
 
 from app import routes, models
 #the module module will define the structure of our database
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -18,10 +20,29 @@ def index():
 	]
 	return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
 	form = LoginForm()
-	return render_template('login.html', title='Sign In', form=form)
-	
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
+		return redirect(url_for('index'))
+	return render_template('login.html', title='Sign in', form=form)
+
+	@app.route('/logout')
+	def logout():
+		logout_user()
+		return redirect(url_for('index'))
+
+		
+
+
+
+
 
 
